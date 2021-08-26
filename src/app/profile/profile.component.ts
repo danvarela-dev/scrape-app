f40143d364988fdde19b98b6c4e4b8749aff5e6f
davaflo <daniel38912@gmail.com>
2021-08-26 01:00:15 -0600

    final commit

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,45 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { user } from '../model/user-model';
 import { UserService } from '../shared/user.service';
 
 @Component({
   selector: 'app-profile',
   template: `
-    <h1 class= "container">GitHub Profile Scraper</h1>
+    <h1 class="container">GitHub Profile Scraper</h1>
 
-<div class="container">
-  <div class = "box">
-    <br>
-    <input id="id" class="input" type="text" placeholder="Github username"  />
-    <button (click)="setId()">Agregar Perfil</button>
-  </div>
-</div>
+    <div class="container">
+      <div class="box">
+        <br />
+        <input
+          id="id"
+          class="input"
+          type="text"
+          placeholder="Github username"
+        />
+        <button (click)="setId()">Agregar Perfil</button>
+      </div>
+    </div>
+    <br />
+    <br />
+    <div *ngFor="let item of list" class="list wrap container col-sm-3">
+      <li >
+        <img src="{{ item.avatar_url }}" alt="pic" align=left />
+        <h2>{{ item?.name }}</h2>
+        <p>{{ item?.company }}</p>
+        <br>
+      </li>
+    </div>
   `,
-  styles: [`
-     .box{
-    border-style: ridge;
-   }
+  styles: [
+    `
+    * {
+        text-align: center;
+        list-style: none;
+      }
 
-   *{
-     text-align:center;
-     margin: 0 auto;
-   }
-  
-  
-  `]
+      .box {
+        border-style: ridge;
+      }
+
+      .list{
+        margin:50px;
+      }
+      
+      
+      img {
+        width: 120px;
+        height: 120px;
+      }
+      
+    `,
+  ],
 })
-
 export class ProfileComponent {
-id;
+  constructor(private usersvc: UserService) {}
+  id;
 
-   constructor(private usersvc: UserService) { }
+  list: any[];
 
- 
-
-
-  setId(){
-   
-    //this.id = (document.getElementById("id") as HTMLInputElement).value;
-    this.usersvc.getUser();
+  setId() {
+    this.id = (document.getElementById('id') as HTMLInputElement).value;
+    this.list = this.usersvc.getUser(this.id);
   }
-
 }
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -2,18 +2,27 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { user } from '../model/model';
+import { user } from '../model/user-model';
+
+
 
 
 @Injectable()
 export class UserService {
+    private listUsers: user[];
  constructor(private http:HttpClient){
-
+    this.listUsers=[];
  }
 
-getUser(id):Observable<user[]>{
-    return this.http.get<user>(`https://api.github.com/users/${id}`);
+getUser(id){
+    
+    this.http.get<user[]>(`https://api.github.com/users/${id}`).subscribe(data=>{
 
+    this.listUsers.push(data);
+    console.log(this.listUsers);    
+});
+
+return this.listUsers;
 }
 
 
